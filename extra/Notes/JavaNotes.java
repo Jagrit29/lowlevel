18175d33e4e835fec8c340457b39fd7680956e19
Jagrit29 <jagritbhupal29@gmail.com>
2024-08-15 18:10:39 +0530

    MultiThreading Notes

--- extra/Notes/JavaNotes.java
+++ extra/Notes/JavaNotes.java
@@ -1,6 +1,14 @@
 package extra.Notes;
 
 public class JavaNotes {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
 }
 
 
