0bcbb1c06a2d0cb3a761981eca0bb24da5cbd2c6
Jagrit29 <jagritbhupal29@gmail.com>
2026-03-09 21:00:47 +0530

    v2begins

---
+++ lowleveldesignv2/inventorysystem/InventoryManager.java
@@ -0,0 +1,7 @@
+package lowleveldesignv2.inventorysystem;
+
+import java.util.HashMap;
+
+public class InventoryManager {
+     HashMap<String, Warehouse> warehouses;
+}
---
+++ lowleveldesignv2/inventorysystem/Warehouse.java
@@ -0,0 +1,8 @@
+package lowleveldesignv2.inventorysystem;
+
+import java.util.HashMap;
+
+public class Warehouse {
+    private String warehouseId;
+    private HashMap<String, Integer> inventory;
+}
