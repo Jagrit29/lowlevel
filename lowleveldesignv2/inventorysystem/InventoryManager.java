fe9e263a8ac9afbfa49c1ee65031028ff9d50331
Jagrit29 <jagritbhupal29@gmail.com>
2026-03-10 19:29:56 +0530

    InventoryManager: methods

--- lowleveldesignv2/inventorysystem/InventoryManager.java
+++ lowleveldesignv2/inventorysystem/InventoryManager.java
@@ -1,7 +1,67 @@
 package lowleveldesignv2.inventorysystem;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
 public class InventoryManager {
      HashMap<String, Warehouse> warehouses;
+
+     public InventoryManager(List<String> warehouseIds) {
+          this.warehouses = new HashMap<>();
+          for(String id: warehouseIds) {
+               warehouses.putIfAbsent(id, new Warehouse(id));
+          }
+     }
+
+     public void addStock(String warehouseId, String productId, int quantity) {
+          if(!warehouses.containsKey(warehouseId)) return; // warehouse not found
+
+          // get warehouse and add stock
+          Warehouse  warehouse = warehouses.get(warehouseId);
+          warehouse.addStock(productId, quantity);
+     }
+
+     public void removeStock(String warehouseId, String productId, int quantity) {
+          if(!warehouses.containsKey(warehouseId)) return;
+
+          // get warehouse and remove stock
+          Warehouse warehouse = warehouses.get(warehouseId);
+          warehouse.removeStock(productId, quantity);
+     }
+
+     public List<Warehouse> getWarehousesWithStock(String productId, int quantity) {
+          List<Warehouse> available = new ArrayList<>();
+          for(Warehouse warehouse: warehouses.values()) {
+               if(warehouse.checkAvailability(productId, quantity)) {
+                    available.add(warehouse);
+               }
+          }
+
+          return available;
+     }
+
+     public void transfer(String fromWarehouseId, String toWarehouseId, String productId, int quantity) {
+          if(quantity <= 0) return; // input validation
+          if(!warehouses.containsKey(fromWarehouseId) || !warehouses.containsKey(toWarehouseId)) return;
+
+          Warehouse fromWh = warehouses.get(fromWarehouseId);
+          Warehouse toWh = warehouses.get(toWarehouseId);
+
+
+          Warehouse first = fromWarehouseId.compareTo(toWarehouseId) < 0 ? fromWh : toWh;
+          Warehouse second = fromWarehouseId.compareTo(toWarehouseId) < 0 ? toWh : fromWh;
+
+          synchronized (first) {
+               synchronized (second) {
+                    if(!fromWh.removeStock(productId, quantity)) return;
+
+                    toWh.addStock(productId, quantity);
+               }
+          }
+     }
 }
--- lowleveldesignv2/inventorysystem/Warehouse.java
+++ lowleveldesignv2/inventorysystem/Warehouse.java
@@ -3,6 +3,26 @@ package lowleveldesignv2.inventorysystem;
 import java.util.HashMap;
 
 public class Warehouse {
-    private String warehouseId;
+    private String id;
     private HashMap<String, Integer> inventory;
+
+    public Warehouse(String id) {
+        this.id = id;
+        this.inventory = new HashMap<>();
+    }
+
+    public void addStock(String productId, int quantity) {
+        inventory.put(productId, inventory.getOrDefault(productId, 0) + quantity);
+    }
+
+    public boolean removeStock(String productId, int quantity) {
+        if(!inventory.containsKey(productId) || inventory.get(productId) < quantity) return false;
+        inventory.put(productId, inventory.getOrDefault(productId, 0) - quantity);
+
+        return true;
+    }
+
+    public boolean checkAvailability(String productId, int quantity) {
+        return inventory.containsKey(productId) && inventory.get(productId) >= quantity;
+    }
 }
